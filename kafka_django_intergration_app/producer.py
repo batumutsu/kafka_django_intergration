30da897c76e6441edc236415272cb7b26b4db510
batumutsu <baptiste.mutuyeyezu@rssb.rw>
2024-12-08 18:18:57 +0200

    Update producer.py

--- kafka_django_intergration_app/producer.py
+++ kafka_django_intergration_app/producer.py
@@ -1,6 +1,4 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
 import time
 import json
 from confluent_kafka import Producer
